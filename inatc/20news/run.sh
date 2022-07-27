7ec945d85b20bade9beb97586b6be925390a67b6
Kevlyn Kadamala <kevlyn@gmail.com>
2022-07-27 11:34:38 +0100

    Old changes

--- inatc/20news/run.sh
+++ inatc/20news/run.sh
@@ -1,6 +1,6 @@
 python neat_run.py -c config.yaml \
- -nc configs/config-1 \
- --train /Volumes/Kevlyn/INATC-Data/reuters/train_pooler.npy \
- --test /Volumes/Kevlyn/INATC-Data/reuters/test_pooler.npy \
- -n 20ng \
- -s 0
+ -nc configs/config \
+ --train /Users/kad99kev/Desktop/INATC-Data/reuters/train_pooler.npy \
+ --test /Users/kad99kev/Desktop/INATC-Data/reuters/test_pooler.npy \
+ -n 20ng_ecoc \
+ -s 2
