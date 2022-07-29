3e0c1b72d1469d49367bfb9c4294d5116ff59dd2
Kevlyn Kadamala <kevlyn@gmail.com>
2022-07-29 10:35:51 +0100

    Made changes to checkpointing

--- inatc/20news/run.sh
+++ inatc/20news/run.sh
@@ -1,19 +1,19 @@
 python neat_run.py -c config.yaml \
- -nc configs/config \
+ -nc config \
  --train data/train_pooler.npy \
  --test data/test_pooler.npy \
  -n 20ng \
  -s 0
 
 #  python neat_run.py -c config.yaml \
-#  -nc configs/config \
+#  -nc config \
 #  --train data/train_pooler.npy \
 #  --test data/test_pooler.npy \
 #  -n 20ng \
 #  -s 1
 
 #  python neat_run.py -c config.yaml \
-#  -nc configs/config \
+#  -nc config \
 #  --train data/train_pooler.npy \
 #  --test data/test_pooler.npy \
 #  -n 20ng \
